ff924a478a0895016dab2b22da67a951d274100e
Iluze25 <153940117+Iluze25@users.noreply.github.com>
2025-03-02 00:02:00 +0700

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
-
+document.getElementById("goToAbout").addEventListener("click", function() {
+    anime({
+        targets: "h1",
+        opacity: [1, 0],
+        duration: 500,
+        easing: "easeInOutQuad",
+        complete: function() {
+            window.location.href = "about.html"; // Pindah halaman
+        }
+    });
+});
 const btn = document.querySelector("button");
 const hoverArea = document.querySelector("div")
 // Efek saat hover
